e27e348f288ed367d72651b42983796c742a1723
Akinsuyiphilip <philipakinbolade@gmail.com>
2022-07-27 12:44:52 +0100

    add files

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+function Contact() {
+return (
+<>
+    <div className="contact">
+        
+    </div>
+</>
+)
+}
+
+export default Contact
